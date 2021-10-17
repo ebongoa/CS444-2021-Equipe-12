a251067c31cb4bf33ab7c644eaffe7ed0250341a
Nicolas <nicolas.grosjean1@etu.esisar.grenoble-inp.fr>
2021-10-17 17:50:39 +0200

    initialiserEnv up. Fonctions a implemanter listee , avec leur regles de grammaires.

--- ProjetCompil/src/fr/esisar/compilation/verif/Verif.java
+++ ProjetCompil/src/fr/esisar/compilation/verif/Verif.java
@@ -31,21 +31,50 @@ public class Verif {
 
    /**
     * Initialisation de l'environnement avec les identificateurs prédéfinis.
+    *
+    * Les identificateurs suivants sont prédéfinis dans le langage JCas :
+
+  		integer, boolean, real (identificateurs de type)
+   		true, false, max_int   (identificateurs de constante)
     */
    private void initialiserEnv() {
       Defn def;
-      // integer
+      // Integer
       def = Defn.creationType(Type.Integer);
       def.setGenre(Genre.PredefInteger);
       env.enrichir("integer", def);
       
-      // ------------
-      // A COMPLETER
-      // ------------
+      // Boolean
+      def = Defn.creationType(Type.Boolean);
+      def.setGenre(Genre.PredefBoolean);
+      env.enrichir("boolean", def);
+      
+      // Real
+      def = Defn.creationType(Type.Real);
+      def.setGenre(Genre.PredefReal);
+      env.enrichir("real", def);
+      
+      // True
+      def = Defn.creationConstBoolean(true);
+      def.setGenre(Genre.PredefTrue);
+      env.enrichir("true", def);
+      
+      // False
+      def = Defn.creationConstBoolean(false);
+      def.setGenre(Genre.PredefFalse);
+      env.enrichir("false", def);
+      
+      // Max_int
+      def = Defn.creationConstInteger(java.lang.Integer.MAX_VALUE);
+      def.setGenre(Genre.PredefMaxInt);
+      env.enrichir("max_int", def);
+
+      
    }
 
    /**************************************************************************
     * PROGRAMME
+    * PROGRAMME 	-> Noeud.Programme(LISTE_DECL, LISTE_INST) 
     **************************************************************************/
    private void verifier_PROGRAMME(Arbre a) throws ErreurVerif {
       initialiserEnv();
@@ -55,21 +84,117 @@ public class Verif {
 
    /**************************************************************************
     * LISTE_DECL
+    * LISTE_DECL 	-> Noeud.ListeDecl(LISTE_DECL, DECL) 
+					|  Noeud.Vide 
     **************************************************************************/
    private void verifier_LISTE_DECL(Arbre a) throws ErreurVerif {
       // A COMPLETER
    }
-
+   
    /**************************************************************************
-    * LISTE_INST
+    * DECL	 	-> Noeud.Decl(LISTE_IDENT, TYPE)
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * LISTE_IDENT 	-> Noeud.ListeIdent(LISTE_IDENT, IDENT) 
+	*				|  Noeud.Vide
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+   *IDENT 		-> Noeud.Ident                   -- attribut de type Chaine
+   **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * TYPE		-> IDENT
+    *			|  TYPE_INTERVALLE
+	*			|  Noeud.Tableau(TYPE_INTERVALLE, TYPE) 
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * TYPE_INTERVALLE -> Noeud.Intervalle(EXP_CONST, EXP_CONST)
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * EXP_CONST 	-> IDENT
+                	|  Noeud.Entier                  -- attribut de type Entier
+					|  Noeud.PlusUnaire(EXP_CONST)
+					|  Noeud.MoinsUnaire(EXP_CONST)
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * LISTE_INST 	-> Noeud.Vide 
+					|  Noeud.ListeInst(LISTE_INST, INST) 
     **************************************************************************/
    private void verifier_LISTE_INST(Arbre a) throws ErreurVerif {
       // A COMPLETER
    }
 
+   /**************************************************************************
+    * INST		-> Noeud.Nop
+				|  Noeud.Affect(PLACE, EXP) 
+				|  Noeud.Pour(PAS, LISTE_INST) 
+				|  Noeud.TantQue(EXP, LISTE_INST) 
+				|  Noeud.Si(EXP, LISTE_INST, LISTE_INST) 
+				|  Noeud.Lecture(PLACE)
+				|  Noeud.Ecriture(LISTE_EXP) 
+                |  Noeud.Ligne
+    **************************************************************************/
+// A COMPLETER  
+   
+   /**************************************************************************
+    * PAS 		-> Noeud.Increment(IDENT, EXP, EXP) 
+				|  Noeud.Decrement(IDENT, EXP, EXP) 	
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * PLACE 		-> IDENT
+					|  Noeud.Index(PLACE, EXP) 
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * LISTE_EXP   	-> Noeud.Vide
+					|  Noeud.ListeExp(LISTE_EXP, EXP) 
+    **************************************************************************/
+// A COMPLETER
+   
+   /**************************************************************************
+    * EXP 		-> Noeud.Et(EXP, EXP) 
+  				|  Noeud.Ou(EXP, EXP) 
+				|  Noeud.Egal(EXP, EXP)
+				|  Noeud.InfEgal(EXP, EXP)
+				|  Noeud.SupEgal(EXP, EXP)
+				|  Noeud.NonEgal(EXP, EXP)
+				|  Noeud.Inf(EXP, EXP)
+				|  Noeud.Sup(EXP, EXP)
+				|  Noeud.Plus(EXP, EXP)
+				|  Noeud.Moins(EXP, EXP)
+				|  Noeud.Mult(EXP, EXP)
+				|  Noeud.DivReel(EXP, EXP)
+				|  Noeud.Reste(EXP, EXP)
+				|  Noeud.Quotient(EXP, EXP)
+				|  Noeud.Index(PLACE, EXP) 
+				|  Noeud.PlusUnaire(EXP) 
+				|  Noeud.MoinsUnaire(EXP) 
+				|  Noeud.Non(EXP)
+				|  Noeud.Conversion(EXP) 
+				|  Noeud.Entier                  -- attribut de type Entier
+				|  Noeud.Reel                    -- attribut de type Reel
+				|  Noeud.Chaine                  -- attribut de type Chaine
+				|  IDENT            
+    **************************************************************************/
+// A COMPLETER   
    // ------------------------------------------------------------------------
    // COMPLETER les operations de vérifications et de décoration pour toutes 
    // les constructions d'arbres
    // ------------------------------------------------------------------------
-
+ 
+   
 }
