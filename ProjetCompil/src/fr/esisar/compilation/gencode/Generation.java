68a155564017120e10e27f9fd5459b9887d5c90c
Nicolas <nicolas.grosjean1@etu.esisar.grenoble-inp.fr>
2021-11-25 23:34:02 +0100

    appel aux Operande.creationOpXX au lieu des New OperandeXX.factorisation des déclarations de Inst,Etiq dans les switch. correction des typo . creation d'une fonction coder_Exp vide.correction des default case.

--- ProjetCompil/src/fr/esisar/compilation/gencode/Generation.java
+++ ProjetCompil/src/fr/esisar/compilation/gencode/Generation.java
@@ -35,150 +35,163 @@ class Generation {
 	   return Etiq.nouvelle(chaine);
    }
 
+   
+   static void coder_Expr(Arbre a, Operande r)
+   {
+	   //@TODO
+   }
+   
+   
+   static void coder_Expr(Arbre a, Registre r)
+   {
+	   //@Placeholder
+   }
+   
    static void coder_Cond(Arbre c, boolean saut, Etiq etiq) {
 	   Noeud noeud = c.getNoeud();
-	   
+	   Inst inst,inst1,inst2 , inst3 ;
+	   Etiq etiq_fin ;
 	   switch(noeud) {
 	   case Ident :
 		   if(c.getChaine().equals(String.valueOf(saut))) {
-			   Inst inst = Inst.creation1(Operation.BRA, new OperandeEtiq(etiq));
+			   inst = Inst.creation1(Operation.BRA, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst);
 		   }
 		   else if(c.getChaine().equals(String.valueOf(!saut))) {
-			   null; // il n'y a rien à faire
+			   return; // il n'y a rien à faire
 		   }
 		   else {
 			   if(saut) {
-				   Inst inst1 = Inst.creation2(Operation.LOAD, new OperandeChaine(c.getChaine()),Operande.R0);
+				   inst1 = Inst.creation2(Operation.LOAD, Operande.creationOpChaine(c.getChaine()),Operande.R0);
 				   Prog.ajouter(inst1);
-				   Inst inst2 = Inst.creation2(Operation.CMP, new OperandeEntier(0), Operande.R0);
+				   inst2 = Inst.creation2(Operation.CMP, Operande.creationOpEntier(0), Operande.R0);
 				   Prog.ajouter(inst2);
-				   Inst inst3 = Inst.creation1(Operation.BNE, new OperandeEtiq(etiq));
+				   inst3 = Inst.creation1(Operation.BNE, Operande.creationOpEtiq(etiq));
 				   Prog.ajouter(inst3);
 			   }
 			   else {
-				   Inst inst1 = Inst.creation2(Operation.LOAD, new OperandeChaine(c.getChaine()),Operande.R0);
+				   inst1 = Inst.creation2(Operation.LOAD, Operande.creationOpChaine(c.getChaine()),Operande.R0);
 				   Prog.ajouter(inst1);
-				   Inst inst2 = Inst.creation2(Operation.CMP, new OperandeEntier(0), Operande.R0);
+				   inst2 = Inst.creation2(Operation.CMP, Operande.creationOpEntier(0), Operande.R0);
 				   Prog.ajouter(inst2);
-				   Inst inst3 = Inst.creation1(Operation.BEQ, new OperandeEtiq(etiq));
+				   inst3 = Inst.creation1(Operation.BEQ, Operande.creationOpEtiq(etiq));
 				   Prog.ajouter(inst3);
 			   }
 		   }
 		   break;
 	   case Et :
 		   if(saut) {
-			   Etiq etiq_fin = Nouvelle_Etiq(etiq.toString()+"_fin");
-			   Coder_Cond(c.getFils1(), false, etiq_fin);
-			   Coder_Cond(c.getFils2(), true, etiq);
+			   etiq_fin = Nouvelle_Etiq(etiq.toString()+"_fin");
+			   coder_Cond(c.getFils1(), false, etiq_fin);
+			   coder_Cond(c.getFils2(), true, etiq);
 			   Prog.ajouter(etiq_fin);
 		   }
 		   else {
-			   Coder_Cond(c.getFils1(), false, etiq);
-			   Coder_Cond(c.getFils2(), false, etiq);
+			   coder_Cond(c.getFils1(), false, etiq);
+			   coder_Cond(c.getFils2(), false, etiq);
 		   }
 		   break;
 	   case Ou :
 		   if(saut) {
-			   Coder_Cond(c.getFils1(), true, etiq);
-			   Coder_Cond(c.getFils2(), true, etiq);
+			   coder_Cond(c.getFils1(), true, etiq);
+			   coder_Cond(c.getFils2(), true, etiq);
 		   }
 		   else {
-			   Etiq etiq_fin = Nouvelle_Etiq(etiq.toString()+"_fin");
-			   Coder_Cond(c.getFils1(), true, etiq_fin);
-			   Coder_Cond(c.getFils2(), false, etiq);
+			   etiq_fin = Nouvelle_Etiq(etiq.toString()+"_fin");
+			   coder_Cond(c.getFils1(), true, etiq_fin);
+			   coder_Cond(c.getFils2(), false, etiq);
 			   Prog.ajouter(etiq_fin);
 		   }
 		   break;
 	   case Non :
-		   Coder_Cond(c.getFils1(), !saut, etiq);
+		   coder_Cond(c.getFils1(), !saut, etiq);
 		   break;
 	   // Opérateurs de comparaison =, <, >, !=, ≤, et ≥
 	   case Egal :
-		   Coder_Expr(c.getFils1(), Registre.R1);
-		   Coder_Expr(c.getFils2(), Registre.R2);
-		   Inst inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
+		   coder_Expr(c.getFils1(), Registre.R1);
+		   coder_Expr(c.getFils2(), Registre.R2);
+		   inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
 		   Prog.ajouter(inst1);
 		   if(saut) {
-			   Inst inst2 = Inst.creation1(Operation.BEQ, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BEQ, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   else {
-			   Inst inst2 = Inst.creation1(Operation.BNE, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BNE, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   break;
 	   case Inf :
-		   Coder_Expr(c.getFils1(), Registre.R1);
-		   Coder_Expr(c.getFils2(), Registre.R2);
-		   Inst inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
+		   coder_Expr(c.getFils1(), Registre.R1);
+		   coder_Expr(c.getFils2(), Registre.R2);
+		   inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
 		   Prog.ajouter(inst1);
 		   if(saut) {
-			   Inst inst2 = Inst.creation1(Operation.BLT, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BLT, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   else {
-			   Inst inst2 = Inst.creation1(Operation.BGE, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BGE, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   break;
 	   case Sup :
-		   Coder_Expr(c.getFils1(), Registre.R1);
-		   Coder_Expr(c.getFils2(), Registre.R2);
-		   Inst inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
+		   coder_Expr(c.getFils1(), Registre.R1);
+		   coder_Expr(c.getFils2(), Registre.R2);
+		   inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
 		   Prog.ajouter(inst1);
 		   if(saut) {
-			   Inst inst2 = Inst.creation1(Operation.BGT, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BGT, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   else {
-			   Inst inst2 = Inst.creation1(Operation.BLE, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BLE, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   break;
 	   case NonEgal :
-		   Coder_Expr(c.getFils1(), Registre.R1);
-		   Coder_Expr(c.getFils2(), Registre.R2);
-		   Inst inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
+		   coder_Expr(c.getFils1(), Registre.R1);
+		   coder_Expr(c.getFils2(), Registre.R2);
+		   inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
 		   Prog.ajouter(inst1);
 		   if(saut) {
-			   Inst inst2 = Inst.creation1(Operation.BNE, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BNE, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   else {
-			   Inst inst2 = Inst.creation1(Operation.BEQ, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BEQ, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   break;
 	   case InfEgal : 
-		   Coder_Expr(c.getFils1(), Registre.R1);
-		   Coder_Expr(c.getFils2(), Registre.R2);
-		   Inst inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
+		   coder_Expr(c.getFils1(), Registre.R1);
+		   coder_Expr(c.getFils2(), Registre.R2);
+		   inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
 		   Prog.ajouter(inst1);
 		   if(saut) {
-			   Inst inst2 = Inst.creation1(Operation.BLE, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BLE, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   else {
-			   Inst inst2 = Inst.creation1(Operation.BGT, new OperandeEtiq(etiq));
+			   Inst inst2 = Inst.creation1(Operation.BGT, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   break;
 	   case SupEgal :
-		   Coder_Expr(c.getFils1(), Registre.R1);
-		   Coder_Expr(c.getFils2(), Registre.R2);
-		   Inst inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
+		   coder_Expr(c.getFils1(), Registre.R1);
+		   coder_Expr(c.getFils2(), Registre.R2);
+		   inst1 = Inst.creation2(Operation.CMP, Operande.R2, Operande.R1);
 		   Prog.ajouter(inst1);
 		   if(saut) {
-			   Inst inst2 = Inst.creation1(Operation.BGE, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BGE, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   else {
-			   Inst inst2 = Inst.creation1(Operation.BLT, new OperandeEtiq(etiq));
+			   inst2 = Inst.creation1(Operation.BLT, Operande.creationOpEtiq(etiq));
 			   Prog.ajouter(inst2);
 		   }
 		   break;
-	   case default :
+	   default:
 		   break;
 	   }
 
@@ -186,20 +199,21 @@ class Generation {
    
    static void Coder_Inst(Arbre a) {
 	   Noeud noeud = a.getNoeud();
-	   
+	   Inst inst,inst1 ;
+	   Etiq etiq_Debut,etiq_Fin,etiq_Cond,etiq_Sinon;
 	   switch(noeud) {
 	   case Si :
-		   Etiq etiq_Fin = Nouvelle_Etiq("Si_Fin");
+		   etiq_Fin = Nouvelle_Etiq("Si_Fin");
 		   if(a.getFils3().getNoeud().equals(Noeud.Vide)) {
-			   Coder_Cond(a.getFils1(), false, etiq_Fin);
+			   coder_Cond(a.getFils1(), false, etiq_Fin);
 			   Coder_Inst(a.getFils2());
 			   Prog.ajouter(etiq_Fin);
 		   }
 		   else {
-			   Etiq etiq_Sinon = Nouvelle_Etiq("Sinon");
-			   Coder_Cond(a.getFils1(), false, etiq_Sinon);
+			   etiq_Sinon = Nouvelle_Etiq("Sinon");
+			   coder_Cond(a.getFils1(), false, etiq_Sinon);
 			   Coder_Inst(a.getFils2());
-			   Inst inst = Inst.creation1(Operation.BRA, new OperandeEtiq(etiq_Fin));
+			   inst = Inst.creation1(Operation.BRA, Operande.creationOpEtiq(etiq_Fin));
 			   Prog.ajouter(inst);
 			   Prog.ajouter(etiq_Sinon);
 			   Coder_Inst(a.getFils3());
@@ -207,26 +221,26 @@ class Generation {
 		   }
 		   break;
 	   case Pour :
-		   Etiq etiq_Cond = Nouvelle_Etiq("Cond");
-		   Etiq etiq_Debut = Nouvelle_Etiq("Debut");
-		   Inst inst = Inst.creation1(Operation.BRA, new OperandeEtiq(etiq_Cond));
+		   etiq_Cond = Nouvelle_Etiq("Cond");
+		   etiq_Debut = Nouvelle_Etiq("Debut");
+		   inst = Inst.creation1(Operation.BRA, Operande.creationOpEtiq(etiq_Cond));
 		   Prog.ajouter(inst);
 		   Prog.ajouter(etiq_Debut);
 		   Coder_Inst(a.getFils2());
 		   Prog.ajouter(etiq_Cond);
-		   Coder_Cond(a.getFils1(), true, new OperandeEtiq(etiq_Debut));
+		   coder_Cond(a.getFils1(), true, Operande.creationOpEtiq(etiq_Debut));
 		   break;
 	   case TantQue :
-		   Etiq etiq_Cond = Nouvelle_Etiq("Cond");
-		   Etiq etiq_Debut = Nouvelle_Etiq("Debut");
-		   Inst inst = Inst.creation1(Operation.BRA, new OperandeEtiq(etiq_Cond));
+		   etiq_Cond = Nouvelle_Etiq("Cond");
+		   etiq_Debut = Nouvelle_Etiq("Debut");
+		   inst = Inst.creation1(Operation.BRA, Operande.creationOpEtiq(etiq_Cond));
 		   Prog.ajouter(inst);
 		   Prog.ajouter(etiq_Debut);
 		   Coder_Inst(a.getFils2());
 		   Prog.ajouter(etiq_Cond);
-		   Coder_Cond(a.getFils1(), true, new OperandeEtiq(etiq_Debut));
+		   coder_Cond(a.getFils1(), true, Operande.creationOpEtiq(etiq_Debut));
 		   break;
-	   case default :
+	   default :
 		   break;
 	   }
 	   
